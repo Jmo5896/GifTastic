c01b078037039c5a525a48501f824091e223bc9a
Jmo5896 <jmo5896@moore-clan.net>
2018-05-07 19:51:49 -0600

    4th commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
     
-// Try using a loop that appends a button for each string in the array.
-
-
 // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
@@ -19,65 +16,86 @@ $(document).ready(function() {
 
 function populateButtons() {
     var buttonsDiv = $('<div>');
+    
     for (var i = 0; i < topics.length; i++) {
         buttonsDiv.append(getButton(topics[i]));
     }
+   
     $('#buttons').html(buttonsDiv);
 }
 
 function getButton(topic) {
     var button = $('<button>');
+   
     button.addClass('topic-button');
-    button.attr('value', topic);
     button.attr('data-topic', topic);
     button.text(topic);
+    
     return button;
 }
 
 $('#buttons').on('click', '.topic-button', function() {
-    //gify population
+    
     $('#gifsGoHere').empty();
 
-    var apiKey = 'Xmjj3tQNWDaq5vc3DK3xgquSrhGcMVdY';
-    var chosenTopic = $(this).val();
-    
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + chosenTopic + '&api_key=' + apiKey + '&limit=10';
+        var apiKey = 'Xmjj3tQNWDaq5vc3DK3xgquSrhGcMVdY';
+    var chosenTopic = $(this).data('topic'); 
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + chosenTopic + '&api_key=' + apiKey + '&limit=10'; //for github pages switched http to https
+
 
     $.ajax({url: queryURL, method: 'GET'})
     .done(function(response) {
-        console.log(response);
-        
+                
         for (var i = 0; i < response.data.length; i++) {
             var gifDiv = $('<div>');
             var ratingsInP = $('<p>').text('Rating: ' + response.data[i].rating);
             var gifImage = $('<img>');
+                        
+            gifImage.attr('src', response.data[i].images.fixed_height_still.url);
+            gifImage.attr('data-still', response.data[i].images.fixed_height_still.url);
+            gifImage.attr('data-animate', response.data[i].images.fixed_height.url);
+            gifImage.attr('data-state', 'still');
+            gifImage.addClass('displayedGif');
 
-            gifImage.attr('src', response.data[i].images.fixed_height.url);
             gifDiv.append(ratingsInP);
             gifDiv.append(gifImage);
 
             $('#gifsGoHere').append(gifDiv);
 
         }
+        console.log(response);
     });
+
 });
 
 $('form').on('submit', function(event) {
+    
     event.preventDefault();
+    
     var userInput = $(this).children('input').val().trim();
+    
     if (!topics.includes(userInput)) {
         topics.push(userInput);
         populateButtons();
     }
+   
     $(this).children('input').val('');
+
+});
+
+$('#gifsGoHere').on('click', '.displayedGif', function() {
+    var stateOfGif = $(this).attr("data-state"); //still or not
+
+    if (stateOfGif === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
 
 var topics = ['funny', 'fail', 'cats'];
 populateButtons();
-
-//giphy api anjax stuff
-
-
-
 
 });
